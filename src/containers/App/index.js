c99341398dfd281e48423eb5d39c456f3029b401
baxtergu <gu.shengnan@ztesoft.com>
2018-06-06 16:46:27 +0800

    参考相关项目建立当前目录结构，可能还需要微调

---
+++ src/containers/App/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import './index.less';
+
+class App extends Component {
+  render() {
+    return <div>App Component Rendered</div>;
+  }
+}
+
+export default App;
